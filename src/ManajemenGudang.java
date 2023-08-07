8ad22e99ede48a757a7683f8ca06422a3fd99df5
Tharissa Najwa <tharissaazz@gmail.com>
2023-08-07 08:56:36 +0700

    Menambahkan lagi beberapa komentar agar lebih jelas

--- src/ManajemenGudang.java
+++ src/ManajemenGudang.java
@@ -34,33 +34,34 @@ public class ManajemenGudang {
         while (loopingMenu) {
             // Menampilkan menu pilihan kepada pengguna
             tampilkanMenu();
+            // Menampilkan input pilihan menu kepada pengguna dengan memanggil fungsi yang sudah dibuat
             int pilihanUser = getPilihanUser(scanner);
 
             switch (pilihanUser) {
                 // Menggunakan switch-case untuk menangani pilihan pengguna
                 case 1:
-                    tambahBarangBaru(scanner, gudang);
+                    tambahBarangBaru(scanner, gudang); // Memanggil fungsi tambah barang baru dengan menyertakan scanner dan arrayList nya(data set)
                     break;
                 case 2:
-                    penguranganStokBarang(scanner, gudang);
+                    penguranganStokBarang(scanner, gudang); // Memanggil fungsi pengurangan stok
                     break;
                 case 3:
-                    penambahanStokBarang(scanner, gudang);
+                    penambahanStokBarang(scanner, gudang); // Memanggil fungsi penambahan stok
                     break;
                 case 4:
-                    lihatStokBarang(gudang);
+                    lihatStokBarang(gudang); // Memanggil fungsi lihat stok barang
                     break;
                 // Keluar dari program
                 case 0:
                     System.out.println("Anda memilih untuk keluar dari program.");
                     System.out.println("Terimakasih atas waktunya.");
                     System.out.println("=====================================================================================");
-                    loopingMenu = false;
+                    loopingMenu = false; // Loop diberhentikan karena program telah selesai
                     break;
                 // Menampilkan pesan kesalahan untuk input yang tidak valid semisalnya huruf/simbol
                 case -1:
                     System.out.println("Maaf! Input pilihan user harus berupa angka! Untuk mencoba lagi, silahkan tekan enter.");
-                    scanner.nextLine();
+                    scanner.nextLine(); // Fungsinya untuk menghapus buffer
                     break;
                 // Menampilkan pesan kesalahan untuk pilihan yang tidak valid misal angka 5 keatas
                 default:
@@ -69,7 +70,7 @@ public class ManajemenGudang {
                     break;
             }
         }
-        scanner.close();
+        scanner.close(); // Fungsinya untuk menghindari kebocoran sumber daya
     }
 
     // Fungsi untuk menampilkan menu kepada pengguna
@@ -90,12 +91,13 @@ public class ManajemenGudang {
     private static int getPilihanUser(Scanner scanner) {
         // Mengambil input pilihan user dan mengembalikan nilainya jika benar/terjadi kesalahan input data
         int pilihanUser;
+        // Mengecek inputan pengguna apakah integer atau bukan
         if (scanner.hasNextInt()) {
             pilihanUser = scanner.nextInt();
-            scanner.nextLine(); // agar tidak terjadi stuck loop
+            scanner.nextLine(); // Untuk membersihkan buffer
         } else {
-            scanner.nextLine(); // agar tidak terjadi stuck loop
-            pilihanUser = -1; // set untuk nilai yang salah
+            scanner.nextLine(); // Untuk membersihkan buffer
+            pilihanUser = -1; // Set untuk nilai yang salah
         }
         return pilihanUser;
     }
@@ -107,7 +109,7 @@ public class ManajemenGudang {
         System.out.print("Silahkan inputkan kategori barang yang diinginkan : ");
         String kategoriBaru = scanner.nextLine();
 
-        // Cek kategori ada atau tidak
+        // Cek kategori sudah ada atau belum
         int categoryIndex = -1;
         boolean categoryExists = false;
         for (int indeksKategori = 0; indeksKategori < gudang.size(); indeksKategori++) {
@@ -123,12 +125,16 @@ public class ManajemenGudang {
         String tambahNamaBarang = scanner.nextLine();
         System.out.print("Silahkan inputkan stok barang yang diinginkan : ");
         String addStokBarang = scanner.nextLine();
+        // Cek agar inputan stok hanya berupa angka 0-9
         if (addStokBarang.matches("^[0-9]+$")) {
+            // Fungsinya untuk bila kategori tidak ada, maka dibuatkan kategori baru
             if (!categoryExists) {
                 gudang.add(new ArrayList<>());
                 categoryIndex = gudang.size() - 1;
             }
+            // Karena sebelumnya input stok berupa string, maka untuk melakukan rumusnya harus dikonversi ke integer terlebih dahulu
             int tambahStokBarang = Integer.parseInt(addStokBarang);
+            // Pengecekan agar stok tidak boleh diinputkan kurang dari 0
             if (tambahStokBarang > 0) {
                 // Cek bila barang sudah ada
                 boolean itemExists = false;
@@ -141,10 +147,13 @@ public class ManajemenGudang {
                         }
                     }
                 }
+                // Bila barang sudah ada, maka datanya tidak akan ditambahkan ke data set
                 if (!itemExists) {
+                    // Menambahkan barang baru pada data set
                     gudang.get(categoryIndex).add(tambahBarang(kategoriBaru, tambahNamaBarang, String.valueOf(tambahStokBarang)));
                     System.out.println("=====================================================================================");
                     System.out.println("Selamat! Anda berhasil menambahkan data barang baru! Berikut update datanya : ");
+                    // Looping 2 kali agar menampilkan kategori, barang, dan stok secara keseluruhan
                     for (int indeksKategori = 0; indeksKategori < gudang.size(); indeksKategori++) {
                         String namaKategori = gudang.get(indeksKategori).get(0).get(0);
                         System.out.printf("\nKategori : %s - %d\n", namaKategori, indeksKategori);
@@ -156,9 +165,11 @@ public class ManajemenGudang {
                         }
                     }
                 } else {
+                    // Menampilkan pesan kesalahan jika barang sudah ada
                     System.out.println("Mohon maaf, barang tersebut sudah ada. Silahkan lihat stok barang yang tersedia.");
                 }
             } else {
+                // Menampilkan pesan kesalahan jika input stok kurang dari 0 dan bukan merupakan angka
                 System.out.println("Maaf! Input stok harus berupa angka dan merupakan bilangan positif! Untuk kembali ke menu, silahkan tekan enter.");
                 scanner.nextLine();
             }
@@ -184,6 +195,7 @@ public class ManajemenGudang {
                     break;
                 }
         }
+        // Jika kategori ada, maka program akan dilanjutkan
         if (categoryExists) {
             System.out.print("Silahkan inputkan nama barang yang diinginkan : ");
             String tambahNamaBarang = scanner.nextLine();
@@ -200,12 +212,16 @@ public class ManajemenGudang {
                 }
             }
 
+            // Jika barang sudah ada, maka program akan dilanjutkan
             if (itemExists) {
                 System.out.print("Silahkan inputkan jumlah stok yang ingin dikurangi : ");
                 String kurangiStokBarang = scanner.nextLine();
+                // Input kurangi stok harus berupa angka
                 if (kurangiStokBarang.matches("^[0-9]+$")) {
                     int minusStock = Integer.parseInt(kurangiStokBarang);
+                    // Input kurangi stok harus lebih dari 0
                     if ((minusStock > 0)) {
+                        // Looping data 2 kali untuk mengambil data barang sesuai dengan inputan kategori dan nama barang oleh pengguna
                         for (int indeksKategori = 0; indeksKategori < gudang.size(); indeksKategori++) {
                             for (int indeksBarang = 0; indeksBarang < gudang.get(indeksKategori).size(); indeksBarang++) {
                                 String namaKategori = gudang.get(indeksKategori).get(indeksBarang).get(0);
@@ -214,15 +230,19 @@ public class ManajemenGudang {
                                 if (barangName.equalsIgnoreCase(tambahNamaBarang)) {
                                     int currentStock = Integer.parseInt(stok);
                                     int updatedStock = currentStock - minusStock;
+                                    // Fungsinya untuk mengecek apakah nilai yang dihasilkan lebih dari 0 atau tidak
                                     if (updatedStock > 0) {
+                                        // Fungsinya untuk mengubah data yang sudah ada agar terupdate
                                         gudang.get(indeksKategori).get(indeksBarang).set(2, String.valueOf(updatedStock));
+                                        // Menampilkan update datanya
                                         System.out.println("=====================================================================================");
                                         System.out.println("Selamat! Anda berhasil mengurangi stok barang! Berikut update datanya : ");
                                         System.out.println("Kategori\t\t\t\tNama Barang\t\t\t\t\t\t\tStok");
                                         System.out.printf("%s - %d\t\t\t\t%s - %d\t\t\t\t\t\t%s\n", namaKategori, indeksKategori, barangName, indeksBarang, updatedStock);
                                         break;
                                     } else {
-                                        System.out.println("Maaf, barang tidak bisa dikurangkan dengan " + minusStock + " karena menghasilkan nilai yang tidak valid.");
+                                        // Menampilkan pesan kesalahan bila hasilnya kurang dari 0
+                                        System.out.println("Maaf, barang tidak bisa dikurangkan dengan " + minusStock + " karena menghasilkan nilai yang tidak valid. Silahkan coba angka yang lebih kecil.");
                                     }
                                 }
 
@@ -240,6 +260,7 @@ public class ManajemenGudang {
                 System.out.println("Maaf! Barang tidak ada, silahkan buat baru.");
             }
         } else {
+            // Menampilkan pesan kesalahan jika kategori tidak ada
             System.out.println("Maaf! Kategori tidak ada, silahkan buat baru.");
         }
     }
@@ -251,6 +272,7 @@ public class ManajemenGudang {
         System.out.print("Silahkan inputkan kategori barang yang ingin dirubah : ");
         String kategoriBaru = scanner.nextLine();
 
+        // Fungsinya untuk mengecek apakah kategori ada atau tidak
         boolean categoryExists = false;
         for (int indeksKategori = 0; indeksKategori < gudang.size(); indeksKategori++) {
             String namaKategori = gudang.get(indeksKategori).get(0).get(0);
@@ -260,6 +282,7 @@ public class ManajemenGudang {
             }
         }
 
+        // Jika kategori ada, maka akan melanjutkan program/perintah selanjutnya
         if (categoryExists) {
             System.out.print("Silahkan inputkan nama barang yang diinginkan : ");
             String tambahNamaBarang = scanner.nextLine();
@@ -276,9 +299,11 @@ public class ManajemenGudang {
                 }
             }
 
+            // Jika barang sudah ada, maka program/perintah selanjutnya akan dilanjutkan
             if (itemExists) {
                 System.out.print("Silahkan inputkan jumlah stok yang ingin ditambah : ");
                 String tambahStok = scanner.nextLine();
+                // Input stok harus berupa angka
                 if (tambahStok.matches("^[0-9]+$")) {
                     int additionalStock = Integer.parseInt(tambahStok);
                     if ((additionalStock > 0)) {
@@ -315,7 +340,6 @@ public class ManajemenGudang {
         } else {
             System.out.println("Maaf! Kategori tidak ada, silahkan buat baru.");
         }
-
     }
 
     // Fungsi untuk melihat stok barang yang tersedia
